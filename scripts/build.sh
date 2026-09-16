38baf001b41b357d9f46b8ce586e19a395f773ad
Jeremy Zheng <jitang.zheng@gmail.com>
2026-09-16 12:33:35 +0000

    :wrench: add support for osx

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,11 +7,30 @@ export VCPKG_DEFAULT_BINARY_CACHE=$PWD/.cache
 
 mkdir -p $VCPKG_DEFAULT_BINARY_CACHE
 
-declare -a targets=("x86_64-linux" "aarch64-linux" "riscv64-linux" "x86_64-windows")
-for i in "${targets[@]}"
-do
-    cmake --preset=$i
-    cmake --build build/$i
-done
+function build_project() {
+    cmake --preset=$1
+    cmake --build build/$1
+}
+
+function build_on_linux() {
+    local -a targets=("x86_64-linux" "aarch64-linux" "riscv64-linux" "x86_64-windows")
+    for i in "${targets[@]}"
+    do
+        build_project $i
+    done
+}
+
+case "$OSTYPE" in
+    linux*)
+        build_on_linux
+        ;;
+    darwin*)
+        build_project "arm64-darwin"
+        ;;
+    *)
+        echo "Unsupported operating system: $OSTYPE"
+        exit 1
+        ;;
+esac
 
 echo "done."
---
+++ scripts/run.bat
@@ -0,0 +1,3 @@
+@echo off
+pansy.exe -d start-proxy-server -P xxxxxx -H host-1
+pause
